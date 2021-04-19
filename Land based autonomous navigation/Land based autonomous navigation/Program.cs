e2fe7fb4f8e740adc18f092043a75883680f94fd
Valery Tikhonov <valerii.tikhonog@gmail.com>
2021-04-19 16:49:44 +0200

    refactored autopilot manager

--- Land based autonomous navigation/Land based autonomous navigation/Program.cs
+++ Land based autonomous navigation/Land based autonomous navigation/Program.cs
@@ -38,7 +38,6 @@ namespace IngameScript
 
         private UIManager uIManager;
         private AutoPilotManager autopilotManager;
-        private WheelController wheelController;
 
         // Constructor
         public Program()
@@ -53,10 +52,7 @@ namespace IngameScript
             uIManager.printOnScreens("autopilot", "Autopilot compiled correctly\nWaiting for system to start", "Autopilot Status");
             uIManager.printOnScreens("hud", "Autopilot compiled correctly\nWaiting for system to start", "HUD");
 
-            wheelController = new WheelController(GridTerminalSystem);
-            autopilotManager = new AutoPilotManager(GridTerminalSystem, wheelController);
-
-            wheelController.ReleaseWheels();
+            autopilotManager = new AutoPilotManager(GridTerminalSystem, uIManager, POWER_FACTOR, PRECISION_FACTOR);
 
             uIManager.printOnScreens("service", "[SYS] Autopilot booted up correctly");
         }
@@ -78,10 +74,18 @@ namespace IngameScript
                 }
                 else
                 {
-                    autopilotManager.onUpdate();
-
-                    uIManager.printOnScreens("autopilot", autopilotManager.DisplayAutopilotInfo());
-                    uIManager.printOnScreens("hud", autopilotManager.DisplayHUD());
+                    if (autopilotManager.onUpdate())
+                    {
+                        Runtime.UpdateFrequency = UpdateFrequency.Update10;
+                        uIManager.printOnScreens("autopilot", autopilotManager.DisplayAutopilotInfo());
+                        uIManager.printOnScreens("hud", autopilotManager.DisplayHUD());
+                    }
+                    else
+                    {
+                        Runtime.UpdateFrequency = UpdateFrequency.Update100;
+                        uIManager.printOnScreens("service", "[AP] Autopilot Disengaged");
+                    }
+                    
                 }
             }
             else
@@ -93,106 +97,6 @@ namespace IngameScript
                 }
                 if (!uIManager.processAction(argument))
                     uIManager.printOnScreens("service", $"[CMD] Unknown command {argument}");
-            }
-
-            // TODO refactor down (move to autopilot man / wheelcontroller / other)
-            // Autopilot
-            if (autopilotManager.remotePilot.IsAutoPilotEnabled)
-            {
-                // If no objective, select one
-                if (autopilotManager.Objective == null)
-                {
-                    List<MyWaypointInfo> route = new List<MyWaypointInfo>();
-                    autopilotManager.remotePilot.GetWaypointInfo(route);
-
-                    if (route.Count > 0)
-                    {
-                        MyWaypointInfo nextWaypoint = route.First();
-                        autopilotManager.Objective = nextWaypoint.Coords;
-                        autopilotManager.remotePilot.HandBrake = false;
-                    }
-                    else
-                    {
-                        // No waypoints in list
-                        autopilotManager.remotePilot.SetAutoPilotEnabled(false);
-                    }
-                    // Speed up clock
-                    Runtime.UpdateFrequency = UpdateFrequency.Update10;
-                }
-                else
-                {
-                    // Objective is selected, navigate to point
-                    // Apply Power
-                    autopilotManager.Cruise(POWER_FACTOR);
-
-                    // Steering calculations
-                    Vector3D relativeTarget = Vector3D.TransformNormal(autopilotManager.Position - autopilotManager.Objective.GetValueOrDefault(), MatrixD.Transpose(controlReference.WorldMatrix));
-                    autopilotManager.Heading = (double)(-Math.Atan2(relativeTarget.X, relativeTarget.Z));
-                    autopilotManager.Steering = wheelController.SteeringDirection(autopilotManager.Heading, autopilotManager.remotePilot.SpeedLimit, autopilotManager.RelativeVelocity.Z);
-
-                    // Distance Calculation
-                    uIManager.printOnScreens("service", "DIST: " + Math.Sqrt(Math.Pow(relativeTarget.X, 2) + Math.Pow(relativeTarget.Z, 2)) + " - " + PRECISION_FACTOR);
-                    if (Math.Sqrt(Math.Pow(relativeTarget.X, 2) + Math.Pow(relativeTarget.Z, 2)) < PRECISION_FACTOR)
-                    {
-                        // Destination has been reached
-                        autopilotManager.Objective = null;
-                        //Update waypoint list
-                        List<MyWaypointInfo> route = new List<MyWaypointInfo>();
-                        autopilotManager.remotePilot.GetWaypointInfo(route);
-
-                        if (route.Count > 0)
-                        {
-                            MyWaypointInfo nextWaypoint = route.First();
-                            route.RemoveAt(0);
-
-                            if (autopilotManager.remotePilot.FlightMode == FlightMode.OneWay)
-                            {
-                                // OneWay mode
-                                autopilotManager.remotePilot.ClearWaypoints();
-                                foreach (MyWaypointInfo wp in route)
-                                {
-                                    autopilotManager.remotePilot.AddWaypoint(wp);
-                                }
-                            }
-                            else if (autopilotManager.remotePilot.FlightMode == FlightMode.Circle)
-                            {
-                                // Circle mode
-                                route.Add(nextWaypoint);
-                                autopilotManager.remotePilot.ClearWaypoints();
-                                foreach (MyWaypointInfo wp in route)
-                                {
-                                    autopilotManager.remotePilot.AddWaypoint(wp);
-                                }
-                            }
-                            else
-                            {
-                                // Patrol
-                                // Not implemented
-                                autopilotManager.remotePilot.SetAutoPilotEnabled(false);
-                                uIManager.printOnScreens("service", "[AP] Patrol not implemented yet!");
-                                return;
-                            }
-
-                            //Need to reset the autopilot status to true after clearing the queue
-                            autopilotManager.remotePilot.SetAutoPilotEnabled(true);
-
-                        }
-
-                        // TODO break after navigation over
-                        uIManager.printOnScreens("service", "[SYS] AP destination reached");
-                    }
-                }
-
-            }
-            else if (Runtime.UpdateFrequency != UpdateFrequency.Update100)
-            {
-                // Autopilot turning off
-                // Slow down clock for better CPU usage
-                Runtime.UpdateFrequency = UpdateFrequency.Update100;
-                autopilotManager.Objective = null;
-                wheelController.ReleaseWheels();
-                //remotePilot.HandBrake = true;  
-                uIManager.printOnScreens("service", "[AP] Autopilot Disengaged");
             }
         }
     }
